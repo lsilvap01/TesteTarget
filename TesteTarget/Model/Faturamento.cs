2a4a1ec653e59d4f202c375df6015df359f68b86
lsilvap01 <lsilvap01@gmail.com>
2025-03-03 18:39:16 -0300

    Solução da Questão 3

---
+++ TesteTarget/Model/Faturamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTarget.Model
+{
+    public class Faturamento
+    {
+        public int dia { get; set; }
+        public float valor { get; set; }
+    }
+}
--- TesteTarget/Program.cs
+++ TesteTarget/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.PortableExecutable;
+using System.Text.Json;
+using System.Xml.Serialization;
+using TesteTarget.Model;
+
 /* Questão 2:
 Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
  */
-
 bool isFibonacci(int n)
 {
     if (n < 0) return false;
@@ -27,3 +32,32 @@ if (nIsFibonacci)
     Console.WriteLine($"O número {n} faz parte da sequência de Fibonacci!");
 else 
     Console.WriteLine($"O número {n} NÃO faz parte da sequência de Fibonacci :(");
+
+
+/* Questão 3:
+Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+ */
+
+void statsFaturamento(Faturamento[] faturamentos)
+{
+    var valoresFaturamento = faturamentos.Select(f => f.valor).ToArray();
+    var menorValorFat = valoresFaturamento.Min();
+    var maiorValorFat = valoresFaturamento.Max();
+    var mediaValorFat = valoresFaturamento.Average();
+    var qtdDiasComFatAcimaDaMedia = valoresFaturamento.Where(v => v > mediaValorFat).Count();
+
+    Console.WriteLine($"Menor valor de faturamento ocorrido: R$ {menorValorFat}");
+    Console.WriteLine($"Maior valor de faturamento ocorrido: R$ {maiorValorFat}");
+    Console.WriteLine($"Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: {qtdDiasComFatAcimaDaMedia}");
+}
+
+// Lê arquivo json com valores de faturamento
+string fonteDadosJson = "Dados/dados.json";
+string dadosJson = File.ReadAllText(fonteDadosJson);
+Faturamento[] faturamentos = JsonSerializer.Deserialize<Faturamento[]>(dadosJson)!;
+
+// Exibe estatísticas do faturamento JSON
+statsFaturamento(faturamentos);
