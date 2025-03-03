9f76fc601299bcd1ba0408b3ba178b96cccf0cd2
lsilvap01 <lsilvap01@gmail.com>
2025-03-03 19:18:09 -0300

    Corrigida solução da questão 3, passando a ignorar dias com faturamento zerado para calculo da média, valor mínimo e valor máximo

--- TesteTarget/Program.cs
+++ TesteTarget/Program.cs
@@ -39,10 +39,16 @@ Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, fa
 • O menor valor de faturamento ocorrido em um dia do mês;
 • O maior valor de faturamento ocorrido em um dia do mês;
 • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+IMPORTANTE: 
+    a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal; 
+    b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média
  */
 
 void statsFaturamento(Faturamento[] faturamentos)
 {
+    // Remove dias com faturamento zerado
+    faturamentos = faturamentos.Where(f => f.valor > 0).ToArray();
+    // Seleciona valores de faturamento
     var valoresFaturamento = faturamentos.Select(f => f.valor).ToArray();
     var menorValorFat = valoresFaturamento.Min();
     var maiorValorFat = valoresFaturamento.Max();
